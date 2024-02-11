25ee2482645f7835ae39224b8e0d06a4754440c3
Myhannad Alsrahen <mohanadalsrahenm1029@gmail.com>
2024-02-11 03:58:31 -0800

    build some feature & make the game more fun

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import random
 from art import close, open
 
 print(close)
-print("Welcome to the number guessing game!\n")
-print("I'm thinking of a number between 0 and 100")
+print("🎉 Welcome to the number guessing game! 🎉\n")
+print("🧠 I'm thinking of a number between 0 and 100")
 random_number = random.randint(0, 100)
 difficulty = input(
-    "Choose the difficulty of the game type 'Easy' or 'Hard' or 'Q' to Quit: "
+    "🤔 Choose the difficulty of the game: 'Easy' or 'Hard' or 'Q' to Quit: "
 ).lower()
 
+# Check if the user has chosen a difficulty level
+if difficulty not in ['easy', 'hard', 'q']:
+    print("❗ Please choose a valid option between 'Easy' or 'Hard' or 'Q' to Quit.")
+    exit()
+
+# Check if the user chose to quit
+if difficulty == 'q':
+    print("👋 Thanks for playing!")
+    exit()
 
 def game_difficulty():
     if difficulty == "easy":
@@ -21,16 +30,18 @@ def game_difficulty():
 lives = game_difficulty()
 
 for number in range(lives, 0, -1):
-    print(f"\nYou have {number} lives left.")
-    guess = int(input("Make a guess: "))
+    print(f"\n🎈 You have {number} lives left.")
+    guess = int(input("🤔 Make a guess: "))
     if guess < random_number:
-        print("Too Low.")
+        print("🔽 Too Low.")
     elif guess > random_number:
-        print("Too High.")
+        print("🔼 Too High.")
     else:
         print(open)
-        print("You win!")
+        print("🎉 You win!")
         break
+else:
+    print(close)
+    print("☹️ Sorry, you ran out of lives. Better luck next time!")
 
-print(f"The number I'm thinking about is: {random_number}")
-
+print(f"🎯 The number I'm thinking about is: {random_number}")
